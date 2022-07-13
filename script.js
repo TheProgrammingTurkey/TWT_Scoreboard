5ab40e7d552bb248c722f29ba04f65752fb093c5
Glitch (glitch-hello-website) <none>
2022-07-13 17:16:28 +0000

    🐩🐞 Checkpoint ./test2.js:31343558/1224 ./test2.html:31343558/38 ./script.js:31343558/526 ./index.html:31343558/46

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
-
-$('#addColumnChild').click(function(){
-   $('#my-table tr').each(function(){
-      $(this).append(`<td></td>`);
-   });
-});
-
 $('#addRowChild').click(function(){
    $('#my-table tbody').append(`<tr>${$('#default-row').html()}</tr>`);
 });
+
+$("#addColumnChild").click(function () {
+    $("#my-table #topRow").append("<th><input placeholder='Name'></th>");
+    $("#my-table #default-row").append("<td></td>");
+  
+});
+
--- test2.js
+++ test2.js
@@ -1,17 +1,10 @@
-
-
-//$('#addColumnChild').click(function(){
- //  $('#my-table tr').each(function(){
- //     $(this).append(`<th><input placeholder="Name"></th>`);
- //  });
-//});
-
 $('#addRowChild').click(function(){
    $('#my-table tbody').append(`<tr>${$('#default-row').html()}</tr>`);
 });
 
 $("#addColumnChild").click(function () {
-    alert("Hello! I am an alert box!!");
-    // Loop over the rows of the table and append the td tag
-    $(".abc tr").append("<tr>col1</tr>");
+    $("#my-table #topRow").append("<th><input placeholder='Name'></th>");
+    $("#my-table #default-row").append("<td></td>");
+  
 });
+
