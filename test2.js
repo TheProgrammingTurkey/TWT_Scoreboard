2d53bf67bfc98a5efef52de2b6c608883faee9d1
Glitch (glitch-hello-website) <none>
2022-07-13 17:05:26 +0000

    🎽🌟 Checkpoint ./test2.js:31343558/605 ./test2.html:31343558/18

--- test2.js
+++ test2.js
@@ -1,11 +1,17 @@
 
 
-$('#addColumnChild').click(function(){
-   $('#my-table tr').each(function(){
-      $(this).append(`<th><input placeholder="Name"></th><td></td>`);
-   });
-});
+//$('#addColumnChild').click(function(){
+ //  $('#my-table tr').each(function(){
+ //     $(this).append(`<th><input placeholder="Name"></th>`);
+ //  });
+//});
 
 $('#addRowChild').click(function(){
    $('#my-table tbody').append(`<tr>${$('#default-row').html()}</tr>`);
+});
+
+$("#addColumnChild").click(function () {
+    alert("Hello! I am an alert box!!");
+    // Loop over the rows of the table and append the td tag
+    $(".abc tr").append("<tr>col1</tr>");
 });
