19b3018ad441725df056ae5b78edb541be7bd1c5
Glitch (glitch-hello-website) <none>
2022-07-14 03:07:35 +0000

    📕🍠 Checkpoint ./test2.js:31343558/363

--- test2.js
+++ test2.js
@@ -6,10 +6,21 @@ $("#addColumnChild").click(function () {
   
 });
 
-$('#addRowChild').click(function(){
-  $('#my-table tbody').append(`<tr>${$('#default-row').html()}</tr>`);
-});
+//$('#addRowChild').click(function(){
+//  $('#my-table tbody').append(`<tr>${$('#default-row').html()}</tr>`);
+//});
 
 $("#deleteColumnChild").click(function () {
   document.getElementById("my-table").deleteRow(-1);
 });
+
+
+
+
+$('#addRowChild').click(function()
+    { var x=document.getElementById("my-table").tBodies[0];  //get the table
+      var node=x.rows[0].cloneNode(true);    //clone the previous node or row
+      x.appendChild(node);   //add the node or row to the table
+    });  
+
+
